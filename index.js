f3826470474cbe316005c2d9baceb8f3e216e616
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-06 14:37:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
+function nestedTarget() {
+  var target = document.querySelector('div.nested div div div div.target');
+  return target
+}
+
