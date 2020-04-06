75704797246402c3f93ed5320d684bdec8eac2c2
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-06 15:42:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rankedList = document.querySelectorAll('.ranked-list');
 
-  for (let i = 0, i < rankedList.length; i++) {
-      let children = rankedList[i].children;
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
 
-      for (let j = 0, j < children.length; j++) {
-        children[j].innerHTML = parseInt(children[j].innerHTML) + n;
-      }
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
   }
 }
